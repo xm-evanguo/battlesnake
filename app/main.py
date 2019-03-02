d6b157e790567eb930c897d34a580f83a4f214d8
Helen Li <lishuwen_ca@hotmail.com>
2019-03-02 15:05:40 -0800

    d

--- app/main.py
+++ app/main.py
@@ -140,14 +140,16 @@ def move():
     bestMove = detector.bestMove(data, simu_map, head_xy, my_next_tail, map_height, map_width)
     print("bestmove is ", bestMove)
 
-'''
+    print("return bestMove")
+    return move_response(bestMove)
+
+    '''
     if my_length < 15 or data["you"]["health"] < 40:
         if direction is not None:
             print("return direction")
             return move_response(direction)
-'''
-    print("return bestMove")
-    return move_response(bestMove)
+            '''
+
 
 @bottle.post('/end')
 def end():
