7ad0d194be6773674aafda2d95190f8bd626d0c8
Zijian Chen <micejian@gmail.com>
2019-03-02 14:32:39 -0800

    hhh

--- app/detector.py
+++ app/detector.py
@@ -3,43 +3,63 @@ import nextmove
 def bestMove(data, map, head_xy, tail_xy, map_height, map_width):
     # 1.no wall, 2.more space, 3.near food
     index = [[0,'up'],[0,'down'],[0,'left'],[0,'right']]
+    my_length = len(data["you"]["body"])
     x = head_xy[0]
     y = head_xy[1]
+    point = 0
+    if my_length < 10:
+        point = 5
+    if my_length >= 10:
+        point = 2
+
 
     for i in range(y-1, -1, -1): #check up
         if map[i][x] < 2:
             index[0][0] += 1
+        if map[i][x] == 1:
+            index[0][0] += point
         else:
             break
     for i in range(y+1, map_height): #check down
         if map[i][x] < 2:
             index[1][0] += 1
+        if map[i][x] == 1:
+            index[1][0] += point
         else:
             break
     for i in range(x-1, -1, -1): #check left
         if map[y][i] < 2:
             index[2][0] += 1
+        if map[i][x] == 1:
+            index[2][0] += point
         else:
             break
     for i in range(x+1, map_width): #check right
         if map[y][i] < 2:
             index[3][0] += 1
+        if map[i][x] == 1:
+            index[3][0] += point
         else:
             break
     print(x,y)
 
-    path = nextmove.shortest_path(map, head_xy, tail_xy)
-    my_length = len(data["you"]["body"])
 
-    if path is None:
-        if 1 <= y < map_height and x < map_width and map[y-1][x] < 2:
-            index[0][0] = index[0][0] - (my_length - 2)
-        if y+1 < map_height and x < map_width and map[y+1][x] < 2:
-            index[1][0] = index[1][0] - (my_length - 2)
-        if y < map_height and 1 <= x < map_width and map[y][x-1] < 2:
-            index[2][0] = index[2][0] - (my_length - 2)
-        if y < map_height and x+1 < map_width and map[y][x+1] < 2:
-            index[3][0] = index[3][0] - (my_length - 2)
+    if 1 <= y < map_height and x < map_width and map[y-1][x] < 2:
+        path = nextmove.shortest_path(map, (x,y-1), tail_xy)
+        if path is None:
+            index[0][0] -= 2
+    if y+1 < map_height and x < map_width and map[y+1][x] < 2:
+        path = nextmove.shortest_path(map, (x,y+1), tail_xy)
+        if path is None:
+            index[1][0] -= 2
+    if y < map_height and 1 <= x < map_width and map[y][x-1] < 2:
+        path = nextmove.shortest_path(map, (x-1,y), tail_xy)
+        if path is None:
+            index[2][0] -= 2
+    if y < map_height and x+1 < map_width and map[y][x+1] < 2:
+        path = nextmove.shortest_path(map, (x+1,y), tail_xy)
+        if path is None:
+            index[3][0] -= 2
 
     if y == 0:
         index[0][0] -= 10000
@@ -69,25 +89,25 @@ def bestMove(data, map, head_xy, tail_xy, map_height, map_width):
 
     if y < map_height and x < map_width and map[y-1][x] >= 2:
         if map[y-1][x] == 7:
-            index[0][0] -= 100
+            index[0][0] -= 5
         else:
             index[0][0] -= 1000
 
     if y+1 < map_height and x < map_width and map[y+1][x] >= 2:
         if map[y+1][x] == 7:
-            index[1][0] -= 100
+            index[1][0] -= 5
         else:
             index[1][0] -= 1000
 
     if y < map_height and x < map_width and map[y][x-1] >= 2:
         if map[y][x-1] == 7:
-            index[2][0] -= 100
+            index[2][0] -= 5
         else:
             index[2][0] -= 1000
 
     if y < map_height and x+1 < map_width and map[y][x+1] >= 2:
         if map[y][x+1] == 7:
-            index[3][0] -= 100
+            index[3][0] -= 5
         else:
             index[3][0] -= 1000
 
